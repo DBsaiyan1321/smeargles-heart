1a1c12e733b3b8940aa95a3fd4bcdecaed54d471
Dorian Brown <devorian143@gmail.com>
2020-08-25 10:21:43 -0700

    Form Submission Spam Fix

--- frontend/components/imaginations/imagination_form.jsx
+++ frontend/components/imaginations/imagination_form.jsx
@@ -6,7 +6,10 @@ import { PongSpinner } from "react-spinners-kit";
 class ImaginationForm extends React.Component { 
     constructor(props) {
         super(props);
-        this.state = this.props.imagination ;
+        this.state = { 
+            imagination: this.props.imagination,
+            clicked: false // I added this later on. If it breaks anything, change it back to just this.state = this.props.imagination
+        };
 
         this.updateField = this.updateField.bind(this);
         this.formSubmission = this.formSubmission.bind(this);
@@ -22,24 +25,28 @@ class ImaginationForm extends React.Component {
 
     updateField(field) { 
         return e => { 
-            this.setState( { [field]: e.target.value })
+            let imagination = this.state.imagination;
+            imagination[field] = e.target.value;
+            this.setState( { imagination })
         }
     }
 
     validateForm() {
-        return (this.state.title.trim() && (this.state.image || this.state.imageFile)) 
+        return (this.state.imagination.title.trim() && (this.state.imagination.image || this.state.imageFile)) 
     }
 
     formSubmission(e) { 
         e.preventDefault();
+        if (this.state.clicked) return;
         const formData = new FormData();
         if (this.validateForm()) { 
+            this.setState({ clicked: true });
             if (this.props.formType === "Edit") { 
                 formData.append('imagination[id]', this.props.imagination.id)
             }
-            formData.append('imagination[title]', this.state.title);
-            formData.append('imagination[description]', this.state.description);
-            formData.append('imagination[artist_id]', this.state.artist_id); 
+            formData.append('imagination[title]', this.state.imagination.title);
+            formData.append('imagination[description]', this.state.imagination.description);
+            formData.append('imagination[artist_id]', this.state.imagination.artist_id); 
             if (this.state.imageFile) {
                 formData.append('imagination[image]', this.state.imageFile);
             } 
@@ -68,6 +75,8 @@ class ImaginationForm extends React.Component {
     }
 
     render() { 
+        // console.log(this.state);
+
         let preview
         
         // The original didn't have the else if, so it would change the image preview if I selected another file on the edit form.
@@ -76,7 +85,7 @@ class ImaginationForm extends React.Component {
         } else if (this.state.imageUrl && this.props.formType === "Edit") { 
             preview = <img src={this.state.imageUrl} />
         } else {
-            preview = (this.state.image && this.props.formType === "Edit") ? <img src={this.state.image} /> : null
+            preview = (this.state.imagination.image && this.props.formType === "Edit") ? <img src={this.state.imagination.image} /> : null
         }
        
         return ( 
@@ -86,9 +95,13 @@ class ImaginationForm extends React.Component {
                 <form onSubmit={this.formSubmission} className="imagination-form">
 
                     <div className="imagination-form-main">
-                        { this.validateForm() 
+                        { 
+                        this.state.clicked 
+                        ? <div className="form-spinner" ><PongSpinner color="#4d4dff" /></div>
+                        : this.validateForm() 
                         ? <input type="submit" value={`${this.props.formType}`} className="submit-imagination-button" />
-                        : <input type="submit" value={`${this.props.formType}`} className="submit-imagination-button faded-out" /> }
+                        : <input type="submit" value={`${this.props.formType}`} className="submit-imagination-button faded-out" /> 
+                        }
 
                         <div className="file-container">
                             <div className="spotted-border-file">
@@ -100,9 +113,9 @@ class ImaginationForm extends React.Component {
                         {/* </div> */}
         
                         <div className="inputs-for-imaginations">
-                            <input onChange={this.updateField("title")} value={this.state.title} className="title-box" rows="1" placeholder="Title" /> {/* placeholder="Give Your Imagination a Title" */}
+                            <input onChange={this.updateField("title")} value={this.state.imagination.title} className="title-box" rows="1" placeholder="Title" /> {/* placeholder="Give Your Imagination a Title" */}
                             <div className="imagination-form-horizontal-bar"></div>
-                            <textarea onChange={this.updateField("description")} value={this.state.description} className="description-box" placeholder="Describe Your Imagination in Words That Will Comprehended By All." />
+                            <textarea onChange={this.updateField("description")} value={this.state.imagination.description} className="description-box" placeholder="Describe Your Imagination in Words That Will Comprehended By All." />
                         </div>
                         
                     </div>
