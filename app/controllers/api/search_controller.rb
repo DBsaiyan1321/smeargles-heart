f66e6cdca8107303c4bf2b23d49dfae8cddc7ef9
Dorian Brown <devorian143@gmail.com>
2020-09-15 14:42:53 -0700

    Search Controller Match Title and Description

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,8 +1,6 @@
 class Api::SearchController < ApplicationController
-    def show 
-        # debugger
-        # @imaginations = Imagination.where('title LIKE :input OR description LIKE :input', input: "%#{params[:id]}%")
-        @imaginations = Imagination.where('title LIKE :input', input: "%#{params[:id]}%")
+    def show
+        @imaginations = Imagination.where('title LIKE :input', input: "%#{params[:id]}%").or(Imagination.where('description LIKE :input', input: "%#{params[:id]}%"));
 
         if @imaginations 
             render "/api/search/show" # file path from the views folder 
--- app/views/api/search/show.json.jbuilder
+++ app/views/api/search/show.json.jbuilder
@@ -1,7 +1,7 @@
 @imaginations.each do |imagination| 
     # debugger
     json.set! imagination.id do 
-        json.extract! imagination, :id, :title
+        json.extract! imagination, :id, :title, :description
         json.image url_for(imagination.image) 
     end 
 end 
