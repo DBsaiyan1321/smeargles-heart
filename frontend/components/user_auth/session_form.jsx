24d36f9a09bd369256d6651016cea763f4972443
Dorian Brown <devorian143@gmail.com>
2020-12-30 09:45:52 -0800

    Session Form to Hooks

--- frontend/components/user_auth/session_form.jsx
+++ frontend/components/user_auth/session_form.jsx
@@ -1,125 +1,111 @@
-import React from "react"; 
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-class SessionForm extends React.Component { 
-    constructor(props) { 
-        super(props);
-        this.state = { 
-            username: "", 
-            email: "", 
-            password: ""
-        };
+const SessionForm = props => {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-        this.demo = this.demo.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.componentWillUnmount = this.componentWillUnmount(this);
-    }
-
-    handleSubmit(e) {
+    const handleSubmit = e => {
         e.preventDefault();
-        const user = Object.assign({}, this.state);
-        this.props.processForm(user)
-    }
+        const user = { username, email, password };
+        props.processForm(user)
+    };
 
-    updateField(field) { 
-        return e => { 
-            this.setState({ [field]: e.target.value });
+    const demo = () => {
+        setUsername("Demo_User") 
+        setPassword("demouser")
+    };
+
+    useEffect(() => { 
+        return () => { 
+            props.clearErrors()
         }
-    }
+    }, []); 
 
-    demo() { 
-        this.setState({ username: "Demo_User", password: "demouser" })
-    }
+    let title;
+    let link;
+    let linkText;
+    if (props.formType === "login") {
+        title = "Log In"
+        link = "/signup"
+        linkText = "Join"
+    } else {
+        title = "Join"
+        link = "/login"
+        linkText = "Log In"
+    };
 
-    componentWillUnmount() { 
-        this.props.clearErrors()
-    }
-
-    render() { 
-        let title; 
-        let link;
-        let linkText;
-        if (this.props.formType === "login") { 
-            title = "Log In"
-            link = "/signup"
-            linkText = "Join"
-        } else { 
-            title = "Join"
-            link="/login"
-            linkText = "Log In"
-        }
-
-        return (
-            <div className="auth-page">
-                <div className="auth-display">
-                    <Link className="auth-display__exit" to="/"><h1>X</h1></Link>
-                    <div className="auth-display__text">
-                        {/* <Link to="/"> */}
-                            <div className="top-text">
-                                <img src="https://smearglesheart-seeds.s3-us-west-1.amazonaws.com/Smeargle-Head.png" alt="logo" className="logo" />
-                                <h3>SMEARGLE'S HEART</h3>
-                            </div>
-                        {/* </Link> */}
-                        <h1>JOIN THE LARGEST CREATIVE BRAIN IN THE REGION</h1>
-                        <p>Whether you want to buy or sell creativity, become a better imaginator, or enjoy the imaginations – you can do it all here.</p>
+    return (
+        <div className="auth-page">
+            <div className="auth-display">
+                <Link className="auth-display__exit" to="/"><h1>X</h1></Link>
+                <div className="auth-display__text">
+                    {/* <Link to="/"> */}
+                    <div className="top-text">
+                        <img src="https://smearglesheart-seeds.s3-us-west-1.amazonaws.com/Smeargle-Head.png" alt="logo" className="logo" />
+                        <h3>SMEARGLE'S HEART</h3>
                     </div>
-                    
+                    {/* </Link> */}
+                    <h1>JOIN THE LARGEST CREATIVE BRAIN IN THE REGION</h1>
+                    <p>Whether you want to buy or sell creativity, become a better imaginator, or enjoy the imaginations – you can do it all here.</p>
+                </div>
 
 
-                    <form onSubmit={e => this.handleSubmit(e)} className="auth-form">
-                        <div className="top-text auth-form__logo">
-                            <img src="https://smearglesheart-seeds.s3-us-west-1.amazonaws.com/Smeargle-Head.png" alt="logo" className="logo" />
-                            <h3>SMEARGLE'S HEART</h3>
+
+                <form onSubmit={e => handleSubmit(e)} className="auth-form">
+                    <div className="top-text auth-form__logo">
+                        <img src="https://smearglesheart-seeds.s3-us-west-1.amazonaws.com/Smeargle-Head.png" alt="logo" className="logo" />
+                        <h3>SMEARGLE'S HEART</h3>
+                    </div>
+
+                    <div className="auth-form__title">
+                        <h1>{title}</h1>
+                        <Link to={link} className="auth-link">{linkText}</Link>
+                    </div>
+
+                    <div className="auth-input-container">
+                        <div className="box">
+                            <input className="input" type="text" value={username} onChange={e => setUsername(e.currentTarget.value)} required />
+                            <label className="label"> Username</label>
                         </div>
 
-                        <div className="auth-form__title">
-                            <h1>{title}</h1>
-                            <Link to={link} className="auth-link">{linkText}</Link>
-                        </div>
-
-                        <div className="auth-input-container">
+                        {(props.formType === "signup") ? (
                             <div className="box">
-                                <input className="input" type="text" value={this.state.username} onChange={this.updateField("username")} required />
-                                <label className="label"> Username</label>
-                            </div>
 
-                            {(this.props.formType === "signup") ? (
-                                <div className="box">
-    
-                                    <input className="input" type="text" value={this.state.email} onChange={this.updateField("email")} required />
-                                    <label className="label"> Email</label>
-    
-                                </div>
-                                ) : <></>
-                            }
+                                <input className="input" type="text" value={email} onChange={e => setEmail(e.currentTarget.value)} required />
+                                <label className="label"> Email</label>
 
-                            <div className="box">
-                                <input className="input" type="password" value={this.state.password} onChange={this.updateField("password")} required />
-                                <label className="label"> Password</label>
                             </div>
+                        ) : <></>
+                        }
+
+                        <div className="box">
+                            <input className="input" type="password" value={password} onChange={e => setPassword(e.currentTarget.value)} required />
+                            <label className="label"> Password</label>
                         </div>
-                        {(this.props.errors) ? (
-                        <div className="auth-errors-container"> 
-                            {this.props.errors.map((error, i) => <p key={i}>{error}</p>)}
-                            <br /> 
-                        </div> 
+                    </div>
+                    {(props.errors) ? (
+                        <div className="auth-errors-container">
+                            {props.errors.map((error, i) => <p key={i}>{error}</p>)}
+                            <br />
+                        </div>
                     ) : <></>}
 
 
 
-                        <div className="buttons">
-                            <button className="auth-button submit">{title.toUpperCase()}</button> 
-                            { (this.props.formType === "login") ? 
-                                <button className="auth-button demo" onClick={() => this.demo()}>DEMO USER</button> :
-                                <></>
-                            }
-                        </div>
-                    </form>
+                    <div className="buttons">
+                        <button className="auth-button submit">{title.toUpperCase()}</button>
+                        {(props.formType === "login") ?
+                            <button className="auth-button demo" onClick={() => demo()}>DEMO USER</button> :
+                            <></>
+                        }
+                    </div>
+                </form>
 
-                </div>
             </div>
-        )
-    }
-}
+        </div>
+    );
+};
 
 export default SessionForm;
