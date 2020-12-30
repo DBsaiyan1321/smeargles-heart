82200e0c2ffc61ed156a4e68f71f742d41bdbd2d
Dorian Brown <devorian143@gmail.com>
2020-12-30 11:10:01 -0800

    Imagination Index and Item to Hooks

--- frontend/components/imaginations/imagination_index.jsx
+++ frontend/components/imaginations/imagination_index.jsx
@@ -1,40 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ImaginationIndexItem from "./imagination_index_item";
 import { WaveSpinner } from "react-spinners-kit";
 
-class ImaginationIndex extends React.Component { 
-    constructor(props) { 
-        super(props)
+const ImaginationIndex = ({ imaginations, fetchImaginations }) => {
+    const [fetched, setFetched] = useState(false);
 
-        this.state = { fetched: false }
-    }
-
-    componentDidMount() { 
+    useEffect(() => {
         window.scrollTo({ top: 0 })
-        this.props.fetchImaginations() 
-            .then(()=> this.setState({fetched: true}))
+        fetchImaginations()
+            .then(() => setFetched(true))
+    }, []); 
+        
+    if (!fetched) {
+        return (
+            <div className="index-spinner-container">
+                <WaveSpinner size={40} color="#4D4DFF" loading={true} />
+            </div >
+        )
+    } else {
+        return (
+            <div className="container">
+                <ul className="imaginations-index">
+                    {imaginations.map(imagination => {
+                        return <ImaginationIndexItem imagination={imagination} key={imagination.id} artist={imagination.username} />
+                    })}
+                </ul>
+            </div>
+        )
     }
-
-    render() {
-        if (!this.state.fetched) {
-            return (
-                <div className="index-spinner-container"> 
-                    <WaveSpinner size={40} color="#4D4DFF" loading={true} />
-                </div > 
-            )
-        } else { 
-            return ( 
-                <div className="container">
-                    <ul className="imaginations-index">
-                        {this.props.imaginations.map(imagination => {
-                            return <ImaginationIndexItem imagination={imagination} key={imagination.id} artist={imagination.username} />
-                        })}
-                    </ul>
-                </div>
-            )
-        }
-    }
-}
+};
 
 // Order: constructor, render, componentDidMount
 
--- frontend/components/imaginations/imagination_index_container.js
+++ frontend/components/imaginations/imagination_index_container.js
@@ -3,8 +3,8 @@ import ImaginationIndex from "./imagination_index";
 import { fetchImaginations } from "../../actions/imagination_actions"
 
 const mapStateToProps = state => ({
-    imaginations: Object.values(state.entities.imaginations).reverse(),
-    users: Object.values(state.entities.users)
+    imaginations: Object.values(state.entities.imaginations).reverse() //, So the latest ones are first. I will implement a filter and sort function later
+    // users: Object.values(state.entities.users)
 });
 
 const mapDispatchToProps = dispatch => ({
