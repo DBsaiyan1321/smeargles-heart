2f5b0686b12530663b2022528fece91dfda4a772
Dorian Brown <devorian143@gmail.com>
2020-12-30 15:07:29 -0800

    Like Button to Hooks

--- frontend/components/likes/like_button.jsx
+++ frontend/components/likes/like_button.jsx
@@ -1,110 +1,81 @@
-import React from "react"; 
+import React, { useState, useEffect } from "react";
 import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 
-class LikeButton extends React.Component {
-    constructor(props) { 
-        super(props)
-        
-        this.state = { likeId: props.existingLikeId, liked: props.liked } // not this.props because I am in the constructor function
+const LikeButton = props => {
+    const [likeId, setLikeId] = useState(props.existingLikeId); 
+    const [liked, setLiked] = useState(props.liked); 
 
-        this.likeOrUnlike = this.likeOrUnlike.bind(this)
-        this.componentCleanup = this.componentCleanup.bind(this)
-    }
-
-    /**************************************************************************/
-    // Got this from stackoverflow, and I like using Vanilla JS for the DOM because it gives me more skills
-
-    componentCleanup() { 
-        if (this.props.currentUser) {
+    const componentCleanup = () => {
+        if (props.currentUser) {
             let like;
-            if (!this.state.likeId) {
-                like = { imagination_id: this.props.imagination.id, user_id: this.props.currentUser.id }
+            if (!likeId) {
+                like = { imagination_id: props.imagination.id, user_id: props.currentUser.id }
             } else {
-                like = { id: this.state.likeId, imagination_id: this.props.imagination.id, user_id: this.props.currentUser.id }
+                like = { id: likeId, imagination_id: props.imagination.id, user_id: props.currentUser.id }
             }
 
-            
-            if (Boolean(this.state.liked && this.props.currentUser && !this.state.likeId)) {
-                this.props.createLike(like)
-            } else if (Boolean(!this.state.liked && this.props.currentUser && this.state.likeId)) {
-                this.props.deleteLike(like)
+
+            if (Boolean(liked && props.currentUser && !likeId)) {
+                props.createLike(like)
+            } else if (Boolean(!liked && props.currentUser && likeId)) {
+                props.deleteLike(like)
             }
         }
     }
 
-    componentDidMount() { 
-        window.addEventListener('beforeunload', this.componentCleanup);
-    }
+    useEffect(() => {
+        window.addEventListener('beforeunload', componentCleanup);
 
-    componentWillUnmount() { 
-        this.componentCleanup();
-        window.removeEventListener('beforeunload', this.componentCleanup);
-    }
+        return () => {
+            componentCleanup();
+            window.removeEventListener('beforeunload', componentCleanup);
+        };
+    }, []);
 
-    /**************************************************************************/
+    useEffect(() => {
+        setLikeId(props.existingLikeId);
+        setLiked(props.liked);
+    }, [props.liked]);
 
-    componentDidUpdate(prevProps) { // Mike's help
-        if (this.props.liked !== prevProps.liked) { 
-            this.setState( { likeId: this.props.existingLikeId, liked: this.props.liked })
+    const likeOrUnlike = e => {
+        if (!props.currentUser) {
+            props.history.push("/login")
         }
-    }
 
-    likeOrUnlike(e) {
-        if (!this.props.currentUser) { 
-            this.props.history.push("/login")
-        }
-        
-        // this.componentCleanup()
-        // this.setState({ liked: !this.state.liked })
-
-        if (this.props.currentUser) {
+        if (props.currentUser) {
             let like;
-            if (!this.props.existingLikeId) {
-                like = { imagination_id: this.props.imagination.id, user_id: this.props.currentUser.id }
+            if (!props.existingLikeId) {
+                like = { imagination_id: props.imagination.id, user_id: props.currentUser.id }
             } else {
-                like = { id: this.props.existingLikeId, imagination_id: this.props.imagination.id, user_id: this.props.currentUser.id }
+                like = { id: props.existingLikeId, imagination_id: props.imagination.id, user_id: props.currentUser.id }
             }
 
-            if (Boolean(!this.props.liked && this.props.currentUser && !this.props.existingLikeId)) {
-                this.props.createLike(like)
-            } else if (Boolean(this.props.liked && this.props.currentUser && this.props.existingLikeId)) {
-                this.props.deleteLike(like)
+            if (Boolean(!props.liked && props.currentUser && !props.existingLikeId)) {
+                props.createLike(like)
+            } else if (Boolean(props.liked && props.currentUser && props.existingLikeId)) {
+                props.deleteLike(like)
             }
         }
     }
 
-    render() { 
-        
-        // return (
-        //     (this.state.liked) 
-        //     ? <div className="like-div" onClick={e => this.likeOrUnlike(e)}> 
-        //             <div className="like-button"><AiFillStar /></div> 
-        //         <h1 className="like-text">LIKED</h1>
-        //       </div>
-        //     : <div className="like-div" onClick={e => this.likeOrUnlike(e)}>
-        //         <div className="like-button"><AiOutlineStar /></div> 
-        //         <h1 className="like-text">LIKE THIS IMAGINATION</h1>
-        //       </div>
-        // )
-        return (
-            (this.props.currentUser) ? (
-                (this.state.liked)
-                    ? <div className="like-div" onClick={e => this.likeOrUnlike(e)}>
-                        <div className="like-button"><AiFillStar /></div>
-                        <h1 className="like-text">LIKED</h1>
-                    </div>
-                    : <div className="like-div" onClick={e => this.likeOrUnlike(e)}>
-                        <div className="like-button"><AiOutlineStar /></div>
-                        <h1 className="like-text">LIKE THIS IMAGINATION</h1>
-                    </div>
-            ) : (
-                    <div className="like-div" onClick={e => this.likeOrUnlike(e)}>
-                        <div className="like-button"><AiOutlineStar /></div>
-                        <h1 className="like-text">LOG IN TO LIKE THIS IMAGINATION</h1>
-                    </div>
-                )
-        )
-    }
+    return (
+        props.currentUser ? (
+            liked
+                ? <div className="like-div" onClick={e => likeOrUnlike(e)}>
+                    <div className="like-button"><AiFillStar /></div>
+                    <h1 className="like-text">LIKED</h1>
+                </div>
+                : <div className="like-div" onClick={e => likeOrUnlike(e)}>
+                    <div className="like-button"><AiOutlineStar /></div>
+                    <h1 className="like-text">LIKE THIS IMAGINATION</h1>
+                </div>
+        ) : (
+                <div className="like-div" onClick={e => likeOrUnlike(e)}>
+                    <div className="like-button"><AiOutlineStar /></div>
+                    <h1 className="like-text">LOG IN TO LIKE THIS IMAGINATION</h1>
+                </div>
+            )
+    )
 };
 
 export default LikeButton;
